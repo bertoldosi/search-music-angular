c8b3022341505ff719b336d84e763a86e2255d6a
bertoldosi <bertoldosi>
2023-09-15 10:50:21 -0300

    feat: Implementando roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { HistoryComponent } from './pages/history/history.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'historico', component: HistoryComponent },
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './components/content/content.component';
+import { HistoryComponent } from './pages/history/history.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, ContentComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ContentComponent, HistoryComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
